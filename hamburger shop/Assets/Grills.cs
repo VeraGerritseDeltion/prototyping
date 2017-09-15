37b2f813f337ccb22678da56fbf1f3f71bb304dc
VeraGerritseDeltion <arev_97@live.nl>
2017-09-15 11:36:09 +0200

    add

---
+++ hamburger shop/Assets/Grills.cs
@@ -0,0 +1,114 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Grills : MonoBehaviour {
+    public List<SingleGrill> itemslotScript = new List<SingleGrill>();
+    public List<GameObject> itemSlot = new List<GameObject>();
+    public List<GameObject> ItemObj = new List<GameObject>();
+
+    public GameObject itemslotGameobject;
+    public int itemslotRows;
+    public Vector3 pos;
+    public GameObject inventory;
+    public SingleGrill itemSelected;
+
+    public Image imageOnMouse;
+    public Hamburger itemToMove;
+    public SingleGrill usedItemslot;
+
+    void Start()
+    {
+        itemslotRows = itemslotRows * 4 - 1;
+        for (int i = 0; i < itemslotRows; i++)
+        {
+            itemSlot.Add(Instantiate(itemslotGameobject, pos, Quaternion.identity) as GameObject);
+            itemSlot[i].transform.parent = inventory.transform;
+            itemslotScript.Add(null);
+            itemslotScript[i] = itemSlot[i].GetComponent<SingleGrill>();
+        }
+    }
+
+    private void Update()
+    {
+        imageOnMouse.transform.position = (new Vector2(Input.mousePosition.x, Input.mousePosition.y));
+        if (Input.GetButtonUp("Fire1"))
+        {
+            OnMouseUpp();
+        }
+    }
+
+
+
+    public void AddItem(Hamburger newItem)
+    {
+        for (int i = 0; i < itemslotScript.Count; i++)
+        {
+            if (newItem != null)
+            {
+                if (itemslotScript[i].item == null)
+                {
+                    itemslotScript[i].item = newItem;
+                    return;
+                }
+            }
+        }
+    }
+
+    public void MoveItem(Hamburger moveItem, SingleGrill usedSlot)
+    {
+        usedItemslot = usedSlot;
+        if (moveItem != null)
+        {
+            imageOnMouse.enabled = true;
+            imageOnMouse.sprite = moveItem.hB2d;
+            itemToMove = moveItem;
+            usedItemslot = usedSlot;
+        }
+    }
+
+
+
+    public void selected(SingleGrill slot)
+    {
+        itemSelected = slot;
+    }
+
+    public void Drop()
+    {
+        if (itemToMove != null && itemSelected != null)
+        {
+            if (itemSelected.item == null)
+            {
+                itemSelected.item = itemToMove;
+                imageOnMouse.enabled = false;
+                usedItemslot.item = null;
+                usedItemslot.selected = false;
+            }
+            else if (itemSelected.item != null)
+            {
+                Hamburger temp = itemSelected.item;
+                itemSelected.item = itemToMove;
+                usedItemslot.item = temp;
+                usedItemslot.selected = false;
+                imageOnMouse.enabled = false;
+            }
+        }
+    }
+
+    public void OnMouseUpp()
+    {
+        if (itemSelected == null && usedItemslot != null)
+        {
+            usedItemslot.selected = false;
+            imageOnMouse.enabled = false;
+        }
+
+        else if (itemSelected == usedItemslot && usedItemslot != null)
+        {
+            usedItemslot.selected = false;
+            imageOnMouse.enabled = false;
+        }
+    }
+}
---
+++ hamburger shop/Assets/Hamburger.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hamburger : MonoBehaviour {
+    public Sprite hB2d;
+    public float rawTime = 2;
+    public float cookedTime=2;
+    public bool cooked;
+    public bool burned;
+    // Use this for initialization
+    void Start () {
+        StartCoroutine(Raw());
+	}
+
+    public IEnumerator Raw()
+    {
+
+        yield return new WaitForSeconds(rawTime);
+        cooked = true;
+        Cooked();
+    }
+
+    public IEnumerator Cooked()
+    {
+        burned = true;
+        yield return new WaitForSeconds(cookedTime);
+    }
+
+
+}
